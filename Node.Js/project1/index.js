414bec0605de517154b082adcbf19f6c54e39225
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-09-05 10:01:06 +0000

    Sep 5, 2026, 3:31 PM

--- Node.Js/project1/index.js
+++ Node.Js/project1/index.js
@@ -28,11 +28,11 @@ app.get("/users",(req,res)=>{
      res.send(html);
 });
 
-app.get("/api/users/:id",(req,res)=>{
+/*app.get("/api/users/:id",(req,res)=>{
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id===id);
     return  res.json(user);
-});
+});*/
 
 app
   .route("/api/users/:id")
