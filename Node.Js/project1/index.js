76af111a72d09dfaa4910fe4255056e99f37f6fb
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-09-05 09:46:48 +0000

    Sep 5, 2026, 3:16 PM

--- Node.Js/project1/index.js
+++ Node.Js/project1/index.js
@@ -53,7 +53,7 @@ app.post("/api/users",(req,res)=>{
     const body=req.body;
     users.push({...body,id:users.length+1});
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        return  res.json({status:"pending"});
+        return  res.json({status:"success",id:users.length});
     })
     
 });
