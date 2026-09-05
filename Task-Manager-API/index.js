3b2ccdb0e14ece908575b8eacc6316daa9f8c76e
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-09-05 10:18:16 +0000

    Sep 5, 2026, 3:48 PM

--- Task-Manager-API/index.js
+++ Task-Manager-API/index.js
@@ -3,4 +3,8 @@ const app=express();//express mein app create krni hai
 app.use(express.json());
 
 const fs=require("fs");//fs ko isliye import kr rhe to save data on json file
+const PORT=9000;
 
+app.listen(PORT,()=>{
+    console.log(`server started at PORT${PORT}`);
+})
