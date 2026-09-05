936fa76ac20094042afa6d6d6001b47834188b90
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-09-05 05:27:33 +0000

    Sep 5, 2026, 10:57 AM

--- Node.Js/project1/index.js
+++ Node.Js/project1/index.js
@@ -29,6 +29,22 @@ app.get("/api/users/:id",(req,res)=>{
     return  res.json(user);
 });
 
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // Edit user with id
+    return res.json({ status: "Pending" });
+  })
+  .delete((req, res) => {
+    return  res.json({status:"pending"});
+});
+
+
 app.patch("/api/users/:id",(req,res)=>{
     //todo :edit the user with id
     return  res.json({status:"pending"});
