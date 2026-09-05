69b4532757e67ea6db1435e901b60afd113c9bb6
ayushgupta2170-A <ayushgupta2170@gmail.com>
2026-09-05 05:11:59 +0000

    Sep 5, 2026, 10:41 AM

--- Node.Js/project1/index.js
+++ Node.Js/project1/index.js
@@ -5,8 +5,25 @@ const app=express();
 const PORT=8000;
 
 //Routes
-app.get("api/users",(req,res)=>{
+app.get("/api/users",(req,res)=>{
     return res.json(users);
 });
 
-app.listen(PORT,()=>console.log(`server started at port `))
+app.get("/users",(req,res)=>{
+    /*      aisa kuch hota hai html tag
+       <ul>
+          <li>ayush gupta</li>
+    */ 
+   
+          
+     const html =`
+    <ul>
+     ${users.map((user)=>`<li>${user.first_name}</li>`)}
+    </ul>
+     `;
+     res.send(html);
+      
+    
+});
+
+app.listen(PORT,()=>console.log(`server started at port ${PORT} `))
